b8427b4348dc033590b6714b27bcd99e7c1d6a52
LJ <lj@eva.com>
2022-09-13 20:56:55 +0800

    update

---
+++ p__newsdesc__id.async.js
@@ -0,0 +1,53 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[462],{99135:function(o,c,F){F.r(c),F.d(c,{default:function(){return D}});var u={topImg:"topImg___zQCgz",center:"center___PEYdN",returnOpt:"returnOpt___KMX74",toTop:"toTop___sN0rx","content-area":"content-area___VbED2","news-detail":"news-detail___AFwdq","news-title":"news-title___N5J8w","news-content":"news-content___bJMq_","news-imgs":"news-imgs___KYuUc","news-imgs-title":"news-imgs-title___zGl5p","hot-news":"hot-news___P2BvS","hot-news-title":"hot-news-title___YD2rW","hot-news-list":"hot-news-list___i8qQl"},i=function(){var s=[{url:"/\u6848\u4F8B\u5C55\u793A/\u7EC49.png",value:"\u901A\u8FBD\u5E02\u9886\u5BFC\u8003\u5BDF\u5361\u5C14\u66FC\u5317\u6597\u81EA\u52A8\u9A7E\u9A76\u519C\u673A\u6570\u5B57\u519C\u4E1A\u79CD\u690D\u670D"},{url:"/\u6848\u4F8B\u5C55\u793A/\u7EC49.png",value:"\u5929\u7A7A\u5730\u4E00\u4F53\u5316\u4FDD\u969C\u5927\u7530\u89C4\u6A21\u79CD\u690D"},{url:"/\u6848\u4F8B\u5C55\u793A/\u7EC49.png",value:"\u65E0\u9521\u5361\u5C14\u66FC\u53C2\u5C55\u65E0\u9521\u4E16\u754C\u7269\u8054\u7F51\u535A\u89C8\u4F1A"}],t="";return s.forEach(function(e){t+=`
+    <div class="`.concat(u["hot-news-list"],`">
+      <img src="`).concat(e.url,`"/>
+      <span title=`).concat(e.value,">").concat(e.value,`</span>
+    </div>
+    `)}),t},B={1:`
+  <div class="`.concat(u["content-area"],`">
+    <div class="`).concat(u["news-detail"],`">
+      <div class="`).concat(u["news-title"],`">
+        \u6211\u56FD\u6210\u529F\u53D1\u5C04\u5409\u6797\u4E00\u53F7\u89C6\u989104\u300105\u300106\u661F
+      </div>
+      <div class="`).concat(u["news-content"],`">
+        \u519B\u62A5\u8BB0\u8005\u592A\u539F11\u670821\u65E5\u7535\uFF08\u8D75\u91D1\u9F99\u3001\u8BB0\u8005\u90B9\u7EF4\u8363\uFF09 
+        \u5317\u4EAC\u65F6\u95F42017\u5E7411\u670821\u65E512\u65F650\u5206\uFF0C\u6211\u56FD\u5728\u592A\u539F\u536B\u661F\u53D1\u5C04\u4E2D\u5FC3\u7528\u957F\u5F81\u516D\u53F7\u8FD0\u8F7D\u706B\u7BAD\uFF0C\u6210\u529F\u53D1\u5C04\u5409\u6797\u4E00\u53F7\u89C6\u989104\u300105\u300106\u661F\u3002
+        \u536B\u661F\u987A\u5229\u8FDB\u5165\u9884\u5B9A\u8F68\u9053\uFF0C\u53D1\u5C04\u4EFB\u52A1\u53D6\u5F97\u5706\u6EE1\u6210\u529F\u3002
+      </div>
+      <div class="`).concat(u["news-imgs"],`">
+        <img src="/\u6848\u4F8B\u5C55\u793A/\u7EC47.png"/>
+      </div>
+      <div class="`).concat(u["news-imgs-title"],`">
+        \u957F\u5F81\u516D\u53F7\u8FD0\u8F7D \u706B\u7BAD\u9759\u5F85\u53D1\u5C04\u3002
+      </div>
+      <div class="`).concat(u["news-content"],`">  
+        \u6B64\u6B21\u53D1\u5C04\u4EFB\u52A1\u662F\u957F\u5149\u536B\u661F\u6280\u672F\u6709\u9650\u516C\u53F8\u81EA\u4E3B\u7814\u53D1\u8D1F\u8D23\u7684\u5546\u4E1A\u9065\u611F\u536B\u661F\u521B\u65B0\u5DE5\u7A0B\u2014\u2014\u5409\u6797\u4E00\u53F7\u536B\u661F\u5DE5\u7A0B\u7684\u7B2C\u4E09\u6B21\u53D1\u5C04\u3002
+        \u8BE5\u5DE5\u7A0B\u753160\u4F59\u9897\u536B\u661F\u7EC4\u6210\u9065\u611F\u89C2\u6D4B\u661F\u5EA7\uFF0C\u5177\u5907\u5BBD\u5E45\u6210\u50CF\u3001\u9AD8\u5206\u6210\u50CF\u3001\u591A\u5149\u8C31\u6210\u50CF\u3001
+        \u89C6\u9891\u6210\u50CF\u7B49\u89C2\u6D4B\u80FD\u529B\uFF0C\u4E3B\u8981\u4E3A\u653F\u5E9C\u90E8\u95E8\u3001\u884C\u4E1A\u7528\u6237\u7B49\u63D0\u4F9B\u9065\u611F\u6570\u636E\u548C\u4EA7\u54C1\u670D\u52A1\u3002
+        \u5409\u6797\u4E00\u53F7\u89C6\u989104\u300105\u300106\u661F\uFF0C\u91C7\u7528\u4E00\u4F53\u5316\u8BBE\u8BA1\uFF0C\u5145\u5206\u7EE7\u627F\u4E86\u89C6\u989103\u661F\u6210\u719F\u5355\u673A\u548C\u6280\u672F\u72B6\u6001\uFF0C
+        \u5E76\u6839\u636E\u5546\u4E1A\u5E02\u573A\u9700\u6C42\u5BF9\u661F\u8F7D\u76F8\u673A\u7CFB\u7EDF\u8FDB\u884C\u4E86\u6027\u80FD\u4F18\u5316\uFF0C\u5C06\u9010\u6B65\u5B8C\u5584\u589E\u5F3A\u661F\u5EA7\u89C2\u6D4B\u80FD\u529B\uFF0C\u66F4\u597D\u5730\u4FC3\u8FDB\u9065\u611F\u884C\u4E1A\u5E94\u7528\u3001\u5546\u4E1A\u5E02\u573A\u63A8\u5E7F\u3002
+      </div>
+      <div class="`).concat(u["news-imgs"],`">
+        <img src="/\u6848\u4F8B\u5C55\u793A/\u7EC47.png"/>
+      </div>
+      <div class="`).concat(u["news-imgs-title"],`">
+        \u957F\u5F81\u516D\u53F7\u8FD0\u8F7D\u706B\u7BAD\u70B9\u706B\u53D1\u5C04
+      </div>
+      <div class="`).concat(u["news-content"],`">  
+        \u957F\u5F81\u516D\u53F7\u8FD0\u8F7D\u706B\u7BAD\u662F\u6211\u56FD\u65B0\u4E00\u4EE3\u4E09\u7EA7\u6DB2\u4F53\u8FD0\u8F7D\u706B\u7BAD\uFF0C\u7531\u4E2D\u56FD\u822A\u5929\u79D1\u6280\u96C6\u56E2\u516C\u53F8\u8D1F\u8D23\u7814\u5236\u751F\u4EA7\u3002
+        \u6B64\u6B21\u53D1\u5C04\uFF0C\u662F\u957F\u5F81\u516D\u53F7\u8FD0\u8F7D\u706B\u7BAD\u7684\u7B2C2\u6B21\u98DE\u884C\uFF0C\u4E5F\u662F\u957F\u5F81\u7CFB\u5217\u8FD0\u8F7D\u706B\u7BAD\u7684
+      </div>
+      <div class="`).concat(u["news-imgs"],`">
+        <img src="/\u6848\u4F8B\u5C55\u793A/\u7EC47.png"/>
+      </div>
+      <div class="`).concat(u["news-imgs-title"],`">
+        \u957F\u5F81\u516D\u53F7\u8FD0\u8F7D\u706B\u7BAD\u76F4\u6307\u82CD\u7A79
+      </div>
+    </div>
+    <div class="`).concat(u["hot-news"],`">
+      <div class="`).concat(u["hot-news-title"],`">
+        \u70ED\u70B9\u65B0\u95FB
+      </div>
+      `).concat(i(),`
+    </div>
+  </div>`)},n=F(62086);function D(a){var s=function(){window.scrollTo({top:0,behavior:"smooth"})};return(0,n.jsxs)("div",{className:u.container,children:[(0,n.jsx)("div",{className:u.topImg,children:(0,n.jsx)("img",{src:"/\u65B0\u95FB\u4E2D\u5FC3/\u7EC41.png"})}),(0,n.jsxs)("div",{className:u.center,children:[(0,n.jsx)("div",{className:u.returnOpt,children:"< \u8FD4\u56DE"}),(0,n.jsx)("div",{className:u.content,dangerouslySetInnerHTML:{__html:B[1]}}),(0,n.jsxs)("div",{className:u.toTop,onClick:s,children:[(0,n.jsx)("img",{src:"/\u65B0\u95FB\u4E2D\u5FC3/\u7EC41.png"}),(0,n.jsx)("span",{children:"\u7F6E\u9876"})]})]})]})}}}]);
